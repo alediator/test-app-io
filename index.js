4a3471c114e95d98e8e6c84ddae172adb6b90d1a
Diaz Torres, Alejandro <alejandro.diaztorres@aareon.com>
2021-04-14 17:13:32 +0200

    First version for the backend

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+var cors = require('cors')
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.use(cors());
+
+// root head
+app.head('/', function(req, res){
+    console.log('Ping received');
+    res.sendStatus(204);
+});
+
+io.on('connection', socket => {
+    console.log('Socket conneted ' + socket.id);
+    socket.on('join-room', (roomId, userId, roomName, userName) => {
+        console.log('User (' + userId + ') "'  + userName + '" joined (' + roomId + ') "' + roomName + '"');
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId);
+            console.log('User (' + userId + ') "'  + userName + '" left (' + roomId + ') "' + roomName + '" (disconnected)');
+        })
+    });
+
+    socket.on('leave-room', (roomId, userId, roomName, userName) => {
+        socket.to(roomId).broadcast.emit('user-disconnected', userId);
+        console.log('User (' + userId + ') "'  + userName + '" left (' + roomId + ') "' + roomName + '"');
+        var clients = io.sockets.adapter.rooms[roomId].sockets;
+        console.log('Users left', clients);
+        socket.disconnect();
+    })
+
+    socket.on("disconnect", (reason) => {
+        console.log('Socket ' + socket.id + ' disconnected because of: ' + reason);
+        console.log('Rooms: ', io.sockets.adapter.rooms);
+    });
+});
+
+
+const port = process.env.NODE_PORT ? process.env.NODE_PORT : 3000;
+
+console.log('Running socket.io in ' + port);
+server.listen(port);
