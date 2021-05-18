bb183597653ef1b627344b34e324f18338c69bee
Sanchez Alonso, Pablo <pablo.sanchezalonso@aareon.com>
2021-05-18 14:12:44 +0200

    fix redis

--- dist/smartvideo/repositories/RedisRepository.js
+++ dist/smartvideo/repositories/RedisRepository.js
@@ -4,12 +4,23 @@ exports.RedisRepository = void 0;
 const redis = require("redis");
 class RedisRepository {
     constructor() {
-        this.client = redis.createClient({
-            port: Number(process.env.REDISPORT),
-            host: process.env.REDISURL,
-            password: process.env.REDISSECRET,
-        });
         this.rooms = new Map();
+        this.client = redis.createClient(Number(process.env.REDISPORT), process.env.REDISURL, { no_ready_check: false });
+        this.client.on('error', function (err) {
+            console.log('redis is not running');
+            console.log(err);
+        });
+        this.client.on('ready', function () {
+            console.log('redis is running');
+        });
+        this.client.auth(process.env.REDISSECRET || '', function (err) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('auth');
+        });
+        this.client.select(0);
     }
     getRoom(roomId) {
         let room = null;
